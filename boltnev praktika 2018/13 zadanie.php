ab39b5982952fa7727193ca56402a6624a3188d6
fannyfan414 <fannyfan414@gmail.com>
2018-05-20 22:54:31 +0200

    praktika pochti getmulti(4) problem

--- boltnev praktika 2018/13 zadanie.php
+++ boltnev praktika 2018/13 zadanie.php
@@ -8,49 +8,71 @@
 
 function foo($n)
 {
+    $massiv=array();
     $k = 0;
-    $i = 1;
-    while ($k <= $n) {
-
-
-        $i++;
-        $k++;
+    $number = 1;
+//    $prime = 2;
+    while ($k < $n) {
+        if(FermEiler(getMultiples($number))){
+           $massiv[]=$number;
+           $k++;
+        }
+        $number+=1;
     }
-
+//    while ($k < $n) {
+//        $prime = gmp_nextprime($prime);
+//        if (gmp_mod($prime, 4) != 1) {
+//            echo "\n".$prime;
+//            $k++;
+//        }
+//
+//    }
+    return [$k,$massiv];
 }
 
 function getMultiples($number)
 {
     static $count;
     static $arr;
-    static $del = 3;
+    static $arrCount;
+    static $del = 2;
 
     if ($number < 1) return false;
     $count = isset($count) ? ++$count : 1;
-    if ($number == 1 && $count == 1) return [1];
+    if ($number == 1 && $count == 1) return [[1],[1]];
     if ($number == 1 && $count > 1) return;
     if (!isset($arr)) $arr = [];
+    if (!isset($arrCount)) $arrCount = [];
 
-    // четные
-    if ($number % 2 == 0) {
-        $arr[] = 2;
-        getMultiples($number / 2);
-        return $arr;
-    }
-
-    // нечетные
     if ($number % $del == 0) {
         $arr[] = $del;
-        getMultiples($number / $del);
-
-        return $arr;
+        $arrCount[$del] += 1;
+        getMultiples(gmp_intval($number / $del));
+        return [$arr, $arrCount];
     }
 
-    $del += 2;
+    $del = (gmp_nextprime($del));
     getMultiples($number);
-
-    return $arr;
+    return [$arr, $arrCount];
 }
 
-$number = 200;
-echo $number . ' = ' . implode('*', getMultiples($number));
+function FermEiler($arr)
+{
+    $numbers = array_unique($arr[0]);
+    $numberCount = $arr[1];
+    $k = 0;
+    $specNumber = 0;
+    while ($specNumber <= $numbers[count($numbers) - 1]) {
+        $specNumber = 4 * $k + 3;
+        if (array_search($specNumber, $numbers)!==false && ($numberCount[$specNumber] % 2 != 0)) {
+            return false;
+        }
+        $k++;
+    }
+    return true;
+}
+
+//$x=foo(4);
+//echo implode(" ",$x[1]);
+//var_dump(foo(3));
+//var_dump(FermEiler(getMultiples(4)));
